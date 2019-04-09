4ff6ffaa6312b15745276c8fd93df1d3d6fb6929
wuchunghsuan <wuchunghsuan@gamil.com>
2019-04-09 18:32:28 +0800

    Update shell

--- shell/functions.sh
+++ shell/functions.sh
@@ -5,6 +5,25 @@ YELLOW="\033[33m"
 BLUE="\033[34m"
 END="\033[0m"
 
+function start_master() {
+	docker run -itd \
+	--name hadoop-master \
+	--network=net1 \
+	--hostname 10.11.0.1 \
+	--ip 10.11.0.1 \
+	-v /home/wuchunghsuan/expose/hadoop-master:/expose \
+	-v /home/wuchunghsuan/expose-conf:/expose/conf \
+	-p 8088:8088 \
+	-p 8030:8030 \
+	-p 8031:8031 \
+	-p 8032:8032 \
+	-p 8033:8033 \
+	-p 9000:9000 \
+	-p 50070:50070 \
+	-p 10020:10020 \
+	wuchunghsuan/hadoop-master
+}
+
 function start_worker() {
 	NAME=$1
 	docker run -itd \
@@ -14,5 +33,6 @@ function start_worker() {
 	--hostname $NAME \
 	--ip $NAME \
 	-v /home/wuchunghsuan/expose/worker-$NAME:/expose \
+	-v /home/wuchunghsuan/expose-conf:/expose/conf \
 	wuchunghsuan/hadoop-worker
 }
--- shell/master-start.sh
+++ shell/master-start.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
-docker run -itd \
---name hadoop-master \
---network=net1 \
---hostname 10.11.0.1 \
---ip 10.11.0.1 \
--p 8088:8088 \
--p 8030:8030 \
--p 8031:8031 \
--p 8032:8032 \
--p 8033:8033 \
--p 9000:9000 \
--p 50070:50070 \
--p 10020:10020 \
-wuchunghsuan/hadoop-master
+. ./functions.sh
 
+start_master
 
