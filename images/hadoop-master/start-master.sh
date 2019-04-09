18c21c1cbbe46363d73be16bf0bb7f5f9dba566d
wuchunghsuan <wuchunghsuan@gamil.com>
2019-04-09 18:19:00 +0800

    Rename hadoop-master

--- images/hadoop-master/start-master.sh
+++ images/hadoop-master/start-master.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+cp /expose/conf/master-conf/* /wuchunghsuan/hadoop-2.8.5/etc/hadoop/
+yes | /wuchunghsuan/hadoop-2.8.5/bin/hdfs namenode -format
+/wuchunghsuan/hadoop-2.8.5/sbin/hadoop-daemon.sh start namenode
 /wuchunghsuan/hadoop-2.8.5/bin/yarn resourcemanager
