e5d4a3af0f229451285631f5ffd4152aa61ad69d
wuchunghsuan <wuchunghsuan@gmail.com>
2019-04-11 15:58:49 +0800

    Update datanode-start.sh & namenode-start.sh

--- shell/datanode-start.sh
+++ shell/datanode-start.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-docker run -itd \
---network=net1 \
---hostname 10.11.0.211 \
---ip 10.11.0.211 \
--v /home/wuchunghsuan/expose/datanode-1:/expose \
--v /home/wuchunghsuan/expose-conf:/expose/conf \
-wuchunghsuan/hadoop-datanode
+. ./functions.sh
+
+start_datanode 10.11.0.211
