a5f6fc7a78d6b8d2bc5a4ac402d9c9f53de83ee5
Wu, ChungHsuan <wuchunghsuan@gmail.com>
2019-04-12 12:41:25 +0800

    Update average.py

--- plot/average.py
+++ plot/average.py
@@ -1,13 +1,25 @@
 
 import sys
+import matplotlib.pyplot as plt
 import numpy as np
 
+
+orage = '#FF8C00'
+light_orange = '#FFA500'
+blue = '#4682B4'
+green = '#2E8B57'
+light_blue = '#87CEFA'
+
 if len(sys.argv) <= 1 :
-    print ("Lack of Job ID.")
+    exit ("Lack of Job ID.")
 jobId = sys.argv[1]
 
 dir = "/home/wuchunghsuan/report-scale-test/"
 filename = dir + jobId + "/report_fetch"
+output = dir + jobId + "/fig_fetch_avg_rate.pdf"
+outputTmp = dir + jobId + "/fetch.tmp"
+# outputFig = "./fig_fetch_avg_rate.pdf"
+# filename = "./report_fetch"
 fi = open(filename)
 
 times = []
@@ -37,9 +49,34 @@ print ("Average time: " + str(np.mean(times)) + " mills")
 print ("Average size: " + str(np.mean(sizes)) + " KB")
 print ("Average rate: " + str(np.mean(rates)) + " MBps")
 
-fo = open("output", "w")
+fo = open(outputTmp, "w")
+
+fo.write(str(np.mean(times)) + "\t" + str(np.mean(sizes)) + "\t" + str(np.mean(rates)))
 
 fo.close()
+
+# fig, ax = plt.subplots()
+# width = 8
+
+# xs = np.arange(len(rates)) * 20
+
+# ax.bar(xs, rates, width, 
+#         linewidth=1, color=blue, 
+#         edgecolor=blue, hatch="++++",
+#         label='Transfer Rate')
+# ax.set_xticks(np.arange(10) * len(rates) * 2)
+# ax.set_xlim(right=(xs[-1] + 20))
+# ax.set_xticklabels(np.arange(10) *len(rates)/10)
+# ax.set_ylim(top=30)
+# # ax.legend(loc=4, fontsize=16, frameon=False)
+# ax.set_ylabel('Transfer Rate (MB/s)', fontsize=12)
+# ax.set_xlabel('Fetcher', fontsize=12)
+# # ax.set_aspect(0.23 / ax.get_data_ratio())
+# # plt.legend(loc=2, fontsize=24, frameon=False)
+
+# plt.savefig(outputFig)
+# size = fig.get_size_inches()
+# print (size)
 
 
 
