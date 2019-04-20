c7b1ca7989f68ee89db7608c97c02ff72d371eda
wuchunghsuan <wuchunghsuan@gmail.com>
2019-04-20 20:40:03 +0800

    Update set-wondershaper.sh

--- shell/set-wondershaper.sh
+++ shell/set-wondershaper.sh
@@ -15,6 +15,7 @@ done
 
 U_LIMIT=240000
 D_LIMIT=240000
+mkdir /var/run/netns
 for CONTAINER_ID in `docker ps -a | grep worker- | awk '{print $1}'`; do
 	set_wondershaper $CONTAINER_ID $U_LIMIT $D_LIMIT
 done
