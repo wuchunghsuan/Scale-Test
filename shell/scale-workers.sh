df860e86cf3df5fe3d862e532e317ddced644213
wuchunghsuan <wuchunghsuan@gmail.com>
2019-04-23 17:14:31 +0800

    Add ip.sh

--- shell/scale-workers.sh
+++ shell/scale-workers.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 FROM=$1
 NUM=$2
+. ./ip.sh
 . ./functions.sh
 
 echo -e "${BLUE}Scale up ${RED}$NUM${BLUE} hadoop workers.${END}"
 
 SIZE=$((${FROM}+${NUM}))
 for ((i=${FROM};i<${SIZE};i++)); do
-        NAME="10.11.0.$i"
-        echo -e "Start ${RED}worker-$NAME${END}"
-        start_worker $NAME
+        NAME="${IP}.$i"
+        echo -e "Start ${RED}worker-$NAME${END} CPU_SET=$(($i - 3))"
+        start_worker $NAME $(($i - 3))
 done
 
 ./set-wondershaper.sh
--- shell/workers-start.sh
+++ shell/workers-start.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 NUM=$1
+. ./ip.sh
 . ./functions.sh
 echo -e "${BLUE}Start ${RED}$NUM${BLUE} hadoop workers.${END}"
 
 SIZE=$((${NUM}+3))
 for ((i=3;i<${SIZE};i++)); do
-	NAME="10.11.1.$i"
+	NAME="${IP}.$i"
 	echo -e "Start ${RED}worker-$NAME${END} CPU_SET=$(($i - 3))"
 	start_worker $NAME $(($i - 3))
 done
