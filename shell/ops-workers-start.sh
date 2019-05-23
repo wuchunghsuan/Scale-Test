ca8cd59e99f6ee218a645e1bfe1a0c10611ac23d
wuchunghsuan <wuchunghsuan@gmail.com>
2019-05-23 15:50:30 +0800

    Add ops-worker-start

---
+++ shell/ops-workers-start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+NUM=$1
+. ./ip.sh
+. ./functions.sh
+echo -e "${BLUE}Start ${RED}$NUM${BLUE} hadoop workers.${END}"
+
+SIZE=$((${NUM}+3))
+for ((i=3;i<${SIZE};i++)); do
+	NAME="${IP}.$i"
+	echo -e "Start ${RED}worker-$NAME${END} CPU_SET=$(($i - 3))"
+	start_ops_worker $NAME $(($i - 3))
+done
+
+./set-wondershaper.sh
