f8d9a50869da618e93b9744914e816a5f82e1b84
wuchunghsuan <wuchunghsuan@gmail.com>
2019-05-23 15:49:59 +0800

    Add images ops-hadoop-worker

--- images/build-images.sh
+++ images/build-images.sh
@@ -3,8 +3,12 @@ function build() {
 	IMAGE=$1
 	docker build -t wuchunghsuan/${IMAGE} ../images/${IMAGE}
 }
-build hadoop-base
-build hadoop-master
-build hadoop-worker
-build hadoop-namenode
-build hadoop-datanode
+#build hadoop-base
+#build hadoop-master
+#build hadoop-worker
+#build hadoop-namenode
+#build hadoop-datanode
+
+#build ops-hadoop-base
+#build ops-hadoop-master
+build ops-hadoop-worker
--- images/ops-hadoop-worker/start-worker.sh
+++ images/ops-hadoop-worker/start-worker.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+/start-yarn.sh &>yarn.log &
+
 cd /wuchunghsuan/OPS/scripts
 ./ops.sh worker start
